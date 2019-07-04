f0e87dc3501cb5fd814ba7489da339e20fbc02db
SakuraRhymes <sakurarhymes@gmail.com>
2019-07-04 16:04:15 +0800

    Update UpdateInfoActivity.java

--- Morra/app/src/main/java/com/exercise/android/morra/UpdateInfoActivity.java
+++ Morra/app/src/main/java/com/exercise/android/morra/UpdateInfoActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
+import android.view.WindowManager;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -26,6 +27,7 @@ public class UpdateInfoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getSupportActionBar().hide();
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_update_info);
 
         edtxtPlayerName = findViewById(R.id.edtxtPlayerName);
